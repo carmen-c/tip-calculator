f48095416343e4c6b81d3968dd1855eaa453a266
carmen-c <carmen.csmwc@gmail.com>
2016-11-11 11:07:47 -0800

    Initial Commit

---
+++ TipCaculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TipCaculator
+//
+//  Created by carmen cheng on 2016-11-11.
+//  Copyright © 2016 carmen cheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TipCaculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TipCaculator
+//
+//  Created by carmen cheng on 2016-11-11.
+//  Copyright © 2016 carmen cheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
