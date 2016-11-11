7cb4422791d4bfa1d20c1afad55428dd96fb496b
carmen-c <carmen.csmwc@gmail.com>
2016-11-11 14:36:14 -0800

    almost done

--- TipCaculator/ViewController.m
+++ TipCaculator/ViewController.m
@@ -10,20 +10,56 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UITextField *billAmountTextField;
+@property (weak, nonatomic) IBOutlet UITextField *tipPercentageTextField;
+@property (weak, nonatomic) IBOutlet UILabel *tipPercentage;
+@property (weak, nonatomic) IBOutlet UILabel *tipAmount;
+@property (weak, nonatomic) IBOutlet UISlider *slider;
+
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardShow:) name:UIKeyboardWillShowNotification object:self.view.window];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardHide:) name:UIKeyboardWillShowNotification object:self.view.window];
+    
+
 }
 
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (IBAction)adjustTipPercentage:(id)sender {
+    self.tipPercentageTextField.text = [NSString stringWithFormat:@"%.f%%", self.slider.value];
+    self.tipPercentage.text = [NSString stringWithFormat:@"%.f%%", self.slider.value];
 }
 
+- (IBAction)calculateTip:(id)sender {
+    float billEntered = [self.billAmountTextField.text floatValue];
+    float tipPercent = [self.tipPercentageTextField.text floatValue];
+    float tip = billEntered * (tipPercent/100);
+    self.tipAmount.text = [NSString stringWithFormat:@"$ %.2f", tip];
+    self.tipPercentage.text = [NSString stringWithFormat:@"%.f%%", tipPercent];
+
+}
+
+#pragma mark - keyboard
+
+-(void) keyboardShow: (NSNotification *)sender{
+    CGSize keyboardSize = [[[sender userInfo] objectForKey:UIKeyboardFrameBeginUserInfoKey] CGRectValue].size;
+    [UIView animateWithDuration:0.3 animations:^{
+        CGRect bill = self.billAmountTextField.frame;
+        bill.size.height -= keyboardSize.height;
+        self.billAmountTextField.frame = bill;
+    }];
+}
+
+-(void) keyboardHide: (NSNotification *)sender{
+    CGSize keyboardSize = [[[sender userInfo] objectForKey:UIKeyboardFrameBeginUserInfoKey] CGRectValue].size;
+    [UIView animateWithDuration:0.3 animations:^{
+    CGRect bill = self.billAmountTextField.frame;
+    bill.size.height -= keyboardSize.height;
+    self.billAmountTextField.frame = bill;
+}];
+}
 
 @end
